5e55dcf0976f943115775d77c6fd03bdf6cde661
dimsam <dimitris.samaras@Schoox.com>
2019-04-16 23:49:22 +0300

    created tweets model/migration/route

---
+++ laravel-app/database/migrations/2019_04_16_184457_create_tweets_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTweetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tweets', function (Blueprint $table) {
+
+            $table->engine = "InnoDB";
+
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned()->nullable();
+            $table->string('text',280);
+            $table->timestamps();
+        });
+
+
+        Schema::table('tweets', function($table) {
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tweets');
+    }
+}
